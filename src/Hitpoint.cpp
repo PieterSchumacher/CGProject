1f6fe871004776d15662792f1b2f4faac017b725
Pieter <pieter.schumacher1@gmail.com>
2020-02-20 14:24:18 +0100

    inline, is it faster though?

--- src/Hitpoint.cpp
+++ src/Hitpoint.cpp
@@ -5,11 +5,10 @@ using std::cout;
 
 bool find_nearest_hitpoint(const vector<shared_ptr<Object>> &objects, const Ray &ray, double offset, Hitpoint &hitpoint) {
     Vector3d n;
-    bool intersect, did_intersect = false;
+    bool did_intersect = false;
     double smallest_t_so_far = 1e6;
     for (const shared_ptr<Object> &object : objects) {
-        intersect = object.get()->intersect(ray, offset, smallest_t_so_far, n);
-        if (intersect) {
+        if (object.get()->intersect(ray, offset, smallest_t_so_far, n)) {
             did_intersect = true;
             hitpoint = {smallest_t_so_far, n, object, ray}; // what to do with object? if the original object gets deleted, will this too?
         }
