abe956a8d81d83e087e88542b7cab3ce42eadedf
Pieter <pieter.schumacher1@gmail.com>
2020-02-26 00:50:06 +0100

    Deliverable 1

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include <Triangle.h>
 #include <Plane.h>
 #include <AABB.h>
+#include <ctime>
+#include <chrono>
 #include "Eigen/Dense"
 #include "omp.h"
 
@@ -31,6 +33,10 @@ int main(int argc, char * argv[]) {
     const double s = 0.003125; // pixel size
     const double plane_offset = 1.0;
     const double gamma = 1.1;
+
+    std::clock_t c_start = std::clock();
+
+    auto t_start = std::chrono::high_resolution_clock::now();
     Camera camera = Camera(
             Vector3d(3,-4,5),
             Vector3d(0,6,0),
@@ -67,9 +73,18 @@ int main(int argc, char * argv[]) {
             rgb_image[2+3*(c+h_res*r)] = pow(clamp(color.b), 1/gamma);
         }
     }
+
+    std::clock_t c_end = std::clock();
+    auto t_end = std::chrono::high_resolution_clock::now();
+    std::cout << std::fixed << "CPU time used: "
+              << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << " ms\n"
+              << "Wall clock time passed: "
+              << std::chrono::duration<double, std::milli>(t_end-t_start).count()
+              << " ms\n";
     // write to file
     std::string filename = "CGDemo";
     write_ppm(filename, rgb_image, h_res, v_res);
+
 }
 
 void initialize_scene(vector<shared_ptr<Object>> &objects, vector<shared_ptr<Light>> &lights) {
@@ -94,6 +109,7 @@ void initialize_scene(vector<shared_ptr<Object>> &objects, vector<shared_ptr<Lig
     sphere3->radius = 1.0;
     sphere3->material = mat3;
     objects.push_back(sphere3);
+
     shared_ptr<Material> mat4(new Material());
     mat4->kd = rgb(1.0,1.0,1.0);
     shared_ptr<Plane> plane1(new Plane());
@@ -101,23 +117,81 @@ void initialize_scene(vector<shared_ptr<Object>> &objects, vector<shared_ptr<Lig
     plane1->normal = Vector3d(0,0,1);
     plane1->material = mat4;
     objects.push_back(plane1);
-//    shared_ptr<Material> mat5(new Material());
-//    mat5->kd = rgb(0.8,0.4,1);
-//    shared_ptr<AABB> aabb1(new AABB());
-//    aabb1->v_min = Vector3d(6,2,0);
-//    aabb1->v_max = Vector3d(9,6,2);
-//    aabb1->material = mat5;
-//    objects.push_back(aabb1);
-//    shared_ptr<Material> mat6(new Material());
-//    mat6->kd = rgb(0.6,0.6,0.2);
-//    shared_ptr<Triangle> triangle1(new Triangle());
-//    triangle1->vertices = Matrix3d();
-//    triangle1->vertices << Vector3d(-10, 0, 0), Vector3d(0, 10, 0), Vector3d(-10, 0, 6);
-//    Vector3d n = -(triangle1->vertices.col(0) - triangle1->vertices.col(1)).cross(triangle1->vertices.col(2) - triangle1->vertices.col(1)).normalized();
-//    triangle1->normals = Matrix3d();
-//    triangle1->normals << n,n,n;
-//    triangle1->material = mat6;
-//    objects.push_back(triangle1);
+
+    shared_ptr<Material> mat5(new Material());
+    mat5->kd = rgb(0.8,0.4,1);
+    shared_ptr<AABB> aabb1(new AABB());
+    aabb1->v_min = Vector3d(6,2,0);
+    aabb1->v_max = Vector3d(9,6,2);
+    aabb1->material = mat5;
+    objects.push_back(aabb1);
+    shared_ptr<Material> mat6(new Material());
+    mat6->kd = rgb(1, 1, 1);
+    shared_ptr<Triangle> triangle1(new Triangle());
+    triangle1->vertices = Matrix3d();
+    triangle1->vertices << Vector3d(-6, 4, 0), Vector3d(-6, 4, 2), Vector3d(-6, 6, 2);
+    Vector3d n = (triangle1->vertices.col(0) - triangle1->vertices.col(1)).cross(triangle1->vertices.col(2) - triangle1->vertices.col(1)).normalized();
+    triangle1->normals = Matrix3d();
+    triangle1->normals << n,n,n;
+    triangle1->material = mat6;
+    objects.push_back(triangle1);
+    shared_ptr<Triangle> triangle2(new Triangle());
+    triangle2->vertices = Matrix3d();
+    triangle2->vertices << Vector3d(-6, 4, 0), Vector3d(-6, 4, 2), Vector3d(-8, 4, 2);
+    n = -(triangle2->vertices.col(0) - triangle2->vertices.col(1)).cross(triangle2->vertices.col(2) - triangle2->vertices.col(1)).normalized();
+    triangle2->normals = Matrix3d();
+    triangle2->normals << n,n,n;
+    triangle2->material = mat6;
+    objects.push_back(triangle2);
+    shared_ptr<Triangle> triangle3(new Triangle());
+    triangle3->vertices = Matrix3d();
+    triangle3->vertices << Vector3d(-6, 4, 0), Vector3d(-8, 6, 2), Vector3d(-8, 4, 2);
+    n = (triangle3->vertices.col(0) - triangle3->vertices.col(1)).cross(triangle3->vertices.col(2) - triangle3->vertices.col(1)).normalized();
+    triangle3->normals = Matrix3d();
+    triangle3->normals << n,n,n;
+    triangle3->material = mat6;
+    objects.push_back(triangle3);
+    shared_ptr<Triangle> triangle4(new Triangle());
+    triangle4->vertices = Matrix3d();
+    triangle4->vertices << Vector3d(-6, 4, 0), Vector3d(-8, 6, 2), Vector3d(-6, 6, 2);
+    n = (triangle4->vertices.col(0) - triangle4->vertices.col(1)).cross(triangle4->vertices.col(2) - triangle4->vertices.col(1)).normalized();
+    triangle4->normals = Matrix3d();
+    triangle4->normals << n,n,n;
+    triangle4->material = mat6;
+    objects.push_back(triangle4);
+    shared_ptr<Triangle> triangle5(new Triangle());
+    triangle5->vertices = Matrix3d();
+    triangle5->vertices << Vector3d(-6, 4, 4), Vector3d(-6, 4, 2), Vector3d(-6, 6, 2);
+    n = -(triangle5->vertices.col(0) - triangle5->vertices.col(1)).cross(triangle5->vertices.col(2) - triangle5->vertices.col(1)).normalized();
+    triangle5->normals = Matrix3d();
+    triangle5->normals << n,n,n;
+    triangle5->material = mat6;
+    objects.push_back(triangle5);
+    shared_ptr<Triangle> triangle6(new Triangle());
+    triangle6->vertices = Matrix3d();
+    triangle6->vertices << Vector3d(-6, 4, 4), Vector3d(-6, 4, 2), Vector3d(-8, 4, 2);
+    n = (triangle6->vertices.col(0) - triangle6->vertices.col(1)).cross(triangle6->vertices.col(2) - triangle6->vertices.col(1)).normalized();
+    triangle6->normals = Matrix3d();
+    triangle6->normals << n,n,n;
+    triangle6->material = mat6;
+    objects.push_back(triangle6);
+    shared_ptr<Triangle> triangle7(new Triangle());
+    triangle7->vertices = Matrix3d();
+    triangle7->vertices << Vector3d(-6, 4, 4), Vector3d(-8, 6, 2), Vector3d(-8, 4, 2);
+    n = (triangle7->vertices.col(0) - triangle7->vertices.col(1)).cross(triangle7->vertices.col(2) - triangle7->vertices.col(1)).normalized();
+    triangle7->normals = Matrix3d();
+    triangle7->normals << n,n,n;
+    triangle7->material = mat6;
+    objects.push_back(triangle7);
+    shared_ptr<Triangle> triangle8(new Triangle());
+    triangle8->vertices = Matrix3d();
+    triangle8->vertices << Vector3d(-6, 4, 4), Vector3d(-8, 6, 2), Vector3d(-6, 6, 2);
+    n = (triangle8->vertices.col(0) - triangle8->vertices.col(1)).cross(triangle8->vertices.col(2) - triangle8->vertices.col(1)).normalized();
+    triangle8->normals = Matrix3d();
+    triangle8->normals << n,n,n;
+    triangle8->material = mat6;
+    objects.push_back(triangle8);
+
     shared_ptr<PointLight> pointLight1(new PointLight());
     pointLight1->I = rgb(253, 40.0, 40.0);
     pointLight1->p = Vector3d(0,4,0) + Vector3d(-3.5,-7,1);
